1ab9880e4af14d59e843313d7a34b3f0b2515094
Ruilin Zoe <zhang.ruil@northeastern.edu>
2022-03-26 01:11:50 -0700

    Update WebserviceApplicationTests.java

--- src/test/java/com/example/webservice/WebserviceApplicationTests.java
+++ src/test/java/com/example/webservice/WebserviceApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.webservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class WebserviceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//     @Test
+//     void contextLoads() {
+//     }
 
 }
