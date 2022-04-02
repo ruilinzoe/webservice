0fb7697e464642eab4069b75fd7e2aedcc22ac7f
Ruilin Zoe <angel2007zoe@gmail.com>
2022-04-01 18:17:57 -0700

    assignment7

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,7 +4,7 @@ sudo yum install -y java-17-openjdk
 sudo yum install -y tomcat
 sudo yum install -y maven
 
-sudo yum install ruby
+sudo yum install ruby -y
 sudo yum install wget
 
 #!/bin/bash
@@ -14,7 +14,7 @@ yum erase codedeploy-agent -y
 
 
 cd /home/ec2-user
-wget https://a7-bucket.s3.us-west-2.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-2.s3.us-west-2.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent status
