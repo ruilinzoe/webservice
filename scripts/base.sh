2c478b4421805074da53eefc38fbedf0041d1034
Ruilin Zoe <angel2007zoe@gmail.com>
2022-04-01 20:35:58 -0700

    work flow update

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,13 +6,6 @@ sudo yum install -y maven
 
 sudo yum install ruby -y
 sudo yum install wget
-
-#!/bin/bash
-CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
-$CODEDEPLOY_BIN stop
-yum erase codedeploy-agent -y
-
-
 cd /home/ec2-user
 wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
 chmod +x ./install
