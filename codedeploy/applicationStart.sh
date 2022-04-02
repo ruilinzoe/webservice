169961459ecf6dd3a956d7f50e57ed84e1ccdba9
Ruilin Zoe <angel2007zoe@gmail.com>
2022-04-02 00:01:25 -0700

    updated code deploy

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,11 +1,1 @@
-#!bin/bash
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
-    -s 
-
-# exit 1
-
 sudo systemctl start tomcat
