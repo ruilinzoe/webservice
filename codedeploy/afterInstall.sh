e5b29d3e0352599f295cf4a1d894769a8db30036
Ruilin Zoe <angel2007zoe@gmail.com>
2022-04-08 18:43:01 -0700

    build rest

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -14,5 +14,5 @@ sudo rm -rf /tmp/logs/*.txt
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
-    -c file:/tmp/cloudwatch-config.json \
+    -c file:./tmp/cloudwatch-config.json \
     -s
