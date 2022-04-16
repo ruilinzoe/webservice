f027647b14ed88bd2729f187cfe2e5a6b2f8f344
Ruilin Zoe <angel2007zoe@gmail.com>
2022-04-16 11:54:21 -0700

    codedeploy

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo systemctl stop tomcat
+
+sudo rm -rf /tmp/webservice-0.0.1-SNAPSHOT
+
+sudo chown tomcat:tomcat /tmp/webservice-0.0.1-SNAPSHOT.jar
+
+# cleanup log files
+sudo rm -rf /tmp/logs/catalina*
+sudo rm -rf /tmp/logs/*.log
+sudo rm -rf /tmp/logs/*.txt
+
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
