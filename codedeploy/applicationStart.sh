e89de2cb3a39366fcda7918c05d4d2039d5558fc
Ruilin Zoe <angel2007zoe@gmail.com>
2022-04-16 13:21:44 -0700

    codedeploy

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -jar /tmp/webservice-0.0.1-SNAPSHOT.jar --spring.config.location=jdbc.properties
+sudo systemctl start tomcat
+
+nohup java -jar /usr/webapp/demoDAO-0.0.1-SNAPSHOT.jar --spring.config.location=application.properties > /dev/null 2> /dev/null < /dev/null & echo $! > ./pid.file &
