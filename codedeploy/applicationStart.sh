7536e9d3267524f7424c5afae873e49484a9e131
Ruilin Zoe <angel2007zoe@gmail.com>
2022-04-16 15:58:42 -0700

    codedeploy

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -3,8 +3,8 @@
 
 sudo systemctl start tomcat
 
-ls 
-echo "Start"
-sudo jobs -l
-sudo nohup java -jar /tmp/webservice-0.0.1-SNAPSHOT.jar --spring.config.location=jdbc.properties > /dev/null 2> /dev/null < /dev/null &
-sudo jobs -l
+# ls 
+# echo "Start"
+# sudo jobs -l
+# sudo nohup java -jar /tmp/webservice-0.0.1-SNAPSHOT.jar --spring.config.location=jdbc.properties > /dev/null 2> /dev/null < /dev/null &
+# sudo jobs -l
