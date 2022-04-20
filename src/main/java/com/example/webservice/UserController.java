1cc011480d9f9e099ba62741369cb3ef4f1efde9
Ruilin Zoe <zhang.ruil@northeastern.edu>
2022-04-20 00:58:10 -0700

    Update UserController.java

--- src/main/java/com/example/webservice/UserController.java
+++ src/main/java/com/example/webservice/UserController.java
@@ -107,7 +107,7 @@ public class UserController {
 
         String returnString = jsonObject.toString();
         logger.info(returnString);
-        return String.format("more test");
+        return String.format("Today is Tuesday");
     }
 
     @PostMapping("/v1/user")
